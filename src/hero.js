c0852ea9aa45f49f00b0219bd31fc1bc1f064ae7
Valentyn Dorosh <wh2ts0n9057@gmail.com>
2019-01-01 23:05:20 +0200

    add first screen animation

---
+++ src/hero.js
@@ -0,0 +1,104 @@
+import { Scene, Controller } from 'scrollmagic'
+import { TimelineMax } from 'gsap'
+
+const why = document.querySelector('.js-why')
+const section = document.querySelector('.js-hero')
+const img = section.querySelector('.js-hero-img')
+const figure = section.querySelector('.js-hero-figure')
+const figureIn = section.querySelector('.js-hero-figure-in')
+const yellow = section.querySelector('.js-hero-yellow')
+const orange = section.querySelector('.js-hero-orange')
+const blue = section.querySelector('.js-hero-blue')
+const groupItemsFirst = section
+	.querySelector('.js-hero-group-first')
+	.querySelectorAll('.js-hero-item')
+const groupItemsSecond = section
+	.querySelector('.js-hero-group-second')
+	.querySelectorAll('.js-hero-item')
+
+const bounceInDuration = 0.9
+const zoomInDuration = 0.4
+const bounceInDelay = '-=0.2'
+const zoomInDelay = '-=0.5'
+const IS_FIXED = 'is-fixed'
+
+new TimelineMax()
+	// group 0
+	.to(yellow, bounceInDuration, {
+		scale: 1,
+		transformOrigin: '50% 50%',
+		svgOrigin: '0 0',
+		ease: Bounce.easeOut
+	})
+	.staggerTo(groupItemsFirst, zoomInDuration, {
+		scale: 1,
+		transformOrigin: '50% 50%'
+	}, 0.15, zoomInDelay)
+	// group 1
+	.to(orange, bounceInDuration, {
+		scale: 1,
+		transformOrigin: '50% 50%',
+		ease: Bounce.easeOut
+	}, bounceInDelay)
+	.staggerTo(groupItemsSecond, zoomInDuration, {
+		scale: 1,
+		transformOrigin: '50% 50%'
+	}, 0.15, zoomInDelay)
+	// group 2
+	.to(blue, bounceInDuration, {
+		scale: 1,
+		transformOrigin: '50% 50%',
+		ease: Bounce.easeOut
+	}, bounceInDelay)
+
+new TimelineMax()
+	.to(img, 0.9, {
+		opacity: 1,
+		x: 0
+	});
+
+(function() {
+	const { scrollY, pageYOffset, getComputedStyle } = window
+	const scrollTop = scrollY || pageYOffset
+
+	const figureOffset = figure.getBoundingClientRect()
+	const figureTop = figureOffset.top + scrollTop
+
+	const whyOffset = why.getBoundingClientRect()
+	const whyBottom = whyOffset.top + scrollTop + why.clientHeight - (parseInt(getComputedStyle(why).paddingBottom) / 2)
+
+	const imgOffset = img.getBoundingClientRect()
+	const imgBottom = imgOffset.top + scrollTop + img.offsetHeight
+
+	const controller = new Controller()
+	const scene = new Scene({
+		offset: figureTop,
+		duration: whyBottom - figureTop - img.offsetHeight
+	})
+	scene
+		.addTo(controller)
+		.on('progress', ({ progress }) => {
+			const maxWidth = `${figure.offsetWidth}px`
+			const minHeight = `${img.offsetHeight}px`
+
+			if (progress > 0 && progress < 1) {
+				figure.classList.add(IS_FIXED)
+				img.style.maxWidth = maxWidth
+				figure.style.minHeight = minHeight
+				figureIn.removeAttribute('style')
+			} else if (progress >= 1) {
+				const diff = whyBottom - figureTop - img.offsetHeight
+				const translateY = `translateY(${diff}px)`
+				figure.classList.remove(IS_FIXED)
+				figureIn.setAttribute('style', `
+					-webkit-transform: ${translateY};
+					-moz-transform: ${translateY};
+					transform: ${translateY};
+				`)
+			} else {
+				figure.classList.remove(IS_FIXED)
+				figureIn.removeAttribute('style')
+			}
+		})
+
+})()
