a3e42e4936df1db0794abc55563fe9c4694c5502
Valentyn Dorosh <wh2ts0n9057@gmail.com>
2019-01-07 20:45:18 +0200

    update

--- src/support.js
+++ src/support.js
@@ -5,12 +5,6 @@ const section = document.querySelector('.js-support')
 const items = section.querySelectorAll('.js-support-item')
 const text = section.querySelectorAll('.js-support-text')
 
-const controller = new Controller()
-const scene = new Scene({
-	triggerHook: 'onLeave',
-	triggerElement: section,
-	duration: 2000
-})
 const tl = new TimelineMax({ paused: true })
 	.staggerTo(items, 0.8, {
 		opacity: 1
@@ -20,13 +14,19 @@ const tl = new TimelineMax({ paused: true })
 		y: 0
 	})
 
-scene
-	.setPin(section)
+const controller = new Controller()
+new Scene({
+	triggerHook: 'onCenter',
+	triggerElement: section,
+	duration: 2000
+})
 	.addTo(controller)
 	.on('progress', ({ progress }) => tl.progress(progress))
-	// .on('enter', () => {
-	// 	tl.play()
-	// })
-	// .on('leave', () => {
-	// 	tl.reverse()
-	// })
+
+new Scene({
+	triggerHook: 'onLeave',
+	triggerElement: section,
+	duration: 2000
+})
+	.addTo(controller)
+	.setPin(section)
