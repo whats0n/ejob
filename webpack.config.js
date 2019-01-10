b6864cb60c6324e77d0f11f190ccb9452925e17b
valentyn.dorosh <valentyn.dorosh@altium.com>
2019-01-10 11:41:32 +0200

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,23 +3,24 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   watch: true,
-  mode: 'development',
+  mode: 'production',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'build/js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread']
+          }
+        }
+      }
+    ]
   }
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.m?js$/,
-  //       exclude: /(node_modules|bower_components)/,
-  //       use: {
-  //         loader: 'babel-loader',
-  //         options: {
-  //           presets: ['@babel/preset-env']
-  //         }
-  //       }
-  //     }
-  //   ]
-  // }
 }
